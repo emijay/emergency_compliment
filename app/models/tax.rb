a3399322791cdd066dbb64dd6a97d806d903d691
Nicholas Bingei <nbinged@gmail.com>
2019-08-19 14:33:06 +0800

    Added tax_percent to tax model

--- app/models/tax.rb
+++ app/models/tax.rb
@@ -1,4 +1,11 @@
 class Tax < ApplicationRecord
-def tax( price )
-  price * 7.5
+
+    def tax( price )
+      price * 7.5
+    end
+
+    def tax_percent ( percent)
+        price * 7.5 / 100
+    end
+
 end
